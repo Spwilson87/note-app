e48a455ba66cbfcefccb773cc64910890c3f21f9
simon wilson <simon.wilson87@outlook.com>
2023-01-16 04:49:12 +0000

    fix port

--- app/config.py
+++ app/config.py
@@ -7,4 +7,4 @@ db_password = os.environ.get('DB_PASSWORD', default='')
 db_port = os.environ.get('DB_PORT', default='5432')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = f"postgresql://notes:notes1234@192.168.1.172:5342/notes"
+SQLALCHEMY_DATABASE_URI = f"postgresql://notes:notes1234@192.168.1.172:5432/notes"
