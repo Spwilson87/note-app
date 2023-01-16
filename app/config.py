1ceecb66d397e6254a216c6faeab0a3ebaec8817
simon wilson <simon.wilson87@outlook.com>
2023-01-16 03:12:43 +0000

    switching to postgres

--- app/config.py
+++ app/config.py
@@ -3,8 +3,8 @@ import os
 db_host = os.environ.get('DB_HOST', default='192.168.1.172')
 db_name = os.environ.get('DB_NAME', default='notes')
 db_user = os.environ.get('DB_USERNAME', default='notes')
-db_password = os.environ.get('DB_PASSWORD', default='notes1234')
-db_port = os.environ.get('DB_PORT', default='3306')
+db_password = os.environ.get('DB_PASSWORD', default='')
+db_port = os.environ.get('DB_PORT', default='5432')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = f"mysql+mysqldb://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+SQLALCHEMSQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"Y_DATABASE_URI = f"mysql+mysqldb://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
