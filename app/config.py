d9a2f52c8a87b801aaa915b51cd3f4d2cfc96872
simon wilson <simon.wilson87@outlook.com>
2023-01-16 02:39:57 +0000

    .

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
 import os
 
-db_host = os.environ.get('DB_HOST', default='localhost')
+db_host = os.environ.get('DB_HOST', default='192.168.1.172')
 db_name = os.environ.get('DB_NAME', default='notes')
 db_user = os.environ.get('DB_USERNAME', default='notes')
-db_password = os.environ.get('DB_PASSWORD', default='')
+db_password = os.environ.get('DB_PASSWORD', default='notes1234')
 db_port = os.environ.get('DB_PORT', default='3306')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
