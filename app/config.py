fbb99162c2e58856176a2231103567df489a6abc
simon wilson <simon.wilson87@outlook.com>
2023-01-16 03:15:35 +0000

    fixed sqlalchemy syntax

--- app/config.py
+++ app/config.py
@@ -7,4 +7,4 @@ db_password = os.environ.get('DB_PASSWORD', default='')
 db_port = os.environ.get('DB_PORT', default='5432')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMSQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"Y_DATABASE_URI = f"mysql+mysqldb://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+SQLALCHEMSQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
