721d4fcea09b681b6baea01141f976db5c62574e
simon wilson <simon.wilson87@outlook.com>
2023-01-16 02:10:01 +0000

    change sqlalchamy cmd

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
-import pymysql
+from flask_sqlalchemy import SQLAlchemy
+
 
 db_host = os.environ.get('DB_HOST', default='localhost')
 db_name = os.environ.get('DB_NAME', default='notes')
@@ -8,4 +9,4 @@ db_password = os.environ.get('DB_PASSWORD', default='')
 db_port = os.environ.get('DB_PORT', default='3306')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = f"mysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
