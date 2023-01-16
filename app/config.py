c59ca83417cc9c8038e066e4f45f8d5af2dbc610
simon wilson <simon.wilson87@outlook.com>
2023-01-16 02:25:59 +0000

    .

--- app/config.py
+++ app/config.py
@@ -1,5 +1,4 @@
 import os
-import mysql.connector
 
 db_host = os.environ.get('DB_HOST', default='localhost')
 db_name = os.environ.get('DB_NAME', default='notes')
@@ -8,4 +7,4 @@ db_password = os.environ.get('DB_PASSWORD', default='')
 db_port = os.environ.get('DB_PORT', default='3306')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = f"mysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+SQLALCHEMY_DATABASE_URI = f"mysql+mysqldb://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
