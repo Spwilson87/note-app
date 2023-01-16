6811bf168a46dc6220e0f32f6a973e32a67a10d5
Spwilson87 <94057901+Spwilson87@users.noreply.github.com>
2023-01-16 16:03:22 +0000

    User auth (#2)
    * add stage to show what git branch is being built
    
    * remove branch stage
    
    * created sign up functionality
    
    * fixed missing / caught by pytest
    
    * fixed test now sign up can POST
    
    * change test name
    
    * fixe db string
    
    * .
    
    * change sqlalchamy cmd
    
    * .
    
    * .
    
    * .
    
    * .
    
    * switching to postgres
    
    * fixed sqlalchemy syntax
    
    * fixed dupe issue
    
    * set to uppercase
    
    * .
    
    * .
    
    * .
    
    * fix port
    
    * added pgadmin
    
    * fixed volume name
    
    * set title
    
    * created routes and html templates for user authentication
    
    Co-authored-by: simon wilson <simon.wilson87@outlook.com>

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask, render_template, redirect, url_for, flash, request
+
+from flask import Flask, render_template, redirect, url_for, flash, request, session
 # flask migrate will create migration scripts to when making changes to db structure
 from flask_migrate import Migrate
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 # This defines some default configs
 
@@ -48,10 +50,37 @@ def create_app(test_config=None):
             flash(error, 'error')
                         
         return render_template('sign_up.html')
-    
-    @app.route('/log_in')
+
+    @app.route('/log_in', methods=('GET', 'POST'))
     def log_in():
-        return 'Login'
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            error = None    
         
-        
+            user = User.query.filter_by(username=username).first()
+            
+            if not user or not check_password_hash(user.password, password):
+                error = 'Username or Password are incorrect'
+            
+            if error is None:
+                session.clear()
+                session['user_id'] = user.id
+                return redirect(url_for('index'))
+            
+            flash(error, 'error')
+
+        return render_template('log_in.html')     
+
+    @app.route('/log_out', methods=('GET', 'DELETE'))
+    def log_out():
+        session.clear()
+        flash('You have logged out', 'success')
+        return redirect(url_for('log_in'))
+
+    @app.route('/')
+    def index():
+        return 'Index'     
+
+
     return app
