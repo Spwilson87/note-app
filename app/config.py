8cc2baf85109e07ee328dfea0aa44df6fe3670ac
simon wilson <simon.wilson87@outlook.com>
2023-01-16 01:50:17 +0000

    fixe db string

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+import pymysql
 
 db_host = os.environ.get('DB_HOST', default='localhost')
 db_name = os.environ.get('DB_NAME', default='notes')
@@ -7,4 +8,4 @@ db_password = os.environ.get('DB_PASSWORD', default='')
 db_port = os.environ.get('DB_PORT', default='3306')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = f"mysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
