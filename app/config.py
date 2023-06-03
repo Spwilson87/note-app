d80458f80ad28fbb2eb579758264d3a411fe46ba
Spwilson87 <94057901+Spwilson87@users.noreply.github.com>
2023-06-03 15:51:31 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
-db_host = os.environ.get('DB_HOST', default='192.168.1.172')
+db_host = os.environ.get('DB_HOST', default='10.0.0.7')
 db_name = os.environ.get('DB_NAME', default='notes')
 db_user = os.environ.get('DB_USERNAME', default='notes')
 db_password = os.environ.get('DB_PASSWORD', default='')
